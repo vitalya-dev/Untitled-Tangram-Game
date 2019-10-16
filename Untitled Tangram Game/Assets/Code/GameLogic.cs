7b03d3ddba3619cb3e2a0c132e0987f688affbf8
Vitalya <vitalya.dev@gmail.com>
2019-10-16 08:55:51 +0300

    Logic Have Gizmo Debug State.

--- Untitled Tangram Game/Assets/Code/GameLogic.cs
+++ Untitled Tangram Game/Assets/Code/GameLogic.cs
@@ -2,8 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class GameLogic : MonoBehaviour {
-    public void clicked(Clickable clickable) {
-        Debug.Log("Click click: " + clickable.name);
+    public GUIStyle gizmo_style = new GUIStyle();
+
+    private enum LogicFSM {
+        GOT_NOTHING,
+        GOT_SHAPE,
+        GOT_FIELD
     }
+    private LogicFSM state = LogicFSM.GOT_NOTHING;
+
+    public void clicked(Clickable clickable) {
+        switch (state) {
+            case LogicFSM.GOT_NOTHING:
+                if (clickable.tag == "Shape")
+                    state = LogicFSM.GOT_SHAPE;
+                break;
+            case LogicFSM.GOT_SHAPE:
+                if (clickable.tag == "Field Piece")
+                    state = LogicFSM.GOT_FIELD;
+                break;
+            case LogicFSM.GOT_FIELD:
+                Debug.Log("Time To Move");
+                break;
+        }
+    }
+
+    void OnDrawGizmos() {
+#if UNITY_EDITOR
+        Handles.Label(transform.position + new Vector3(0.0f, 0.0f, 0), "GameLogic: " + state.ToString(), gizmo_style);
+#endif
+    }
+
 }
