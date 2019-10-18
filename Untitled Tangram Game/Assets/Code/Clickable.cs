af28bf4c00d901cd3a2a40e39d348a9486bef958
Vitalya <vitalya.dev@gmail.com>
2019-10-18 09:04:35 +0300

    Docking Points

--- Untitled Tangram Game/Assets/Code/Clickable.cs
+++ Untitled Tangram Game/Assets/Code/Clickable.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.Events;
 
 [System.Serializable]
-public class ClickUnityEvent : UnityEvent<Clickable> { }
+public class ClickUnityEvent : UnityEvent<Clickable, Vector2> { }
 
 public class Clickable : MonoBehaviour {
     public ClickUnityEvent click_callback = new ClickUnityEvent();
@@ -15,12 +15,12 @@ public class Clickable : MonoBehaviour {
             Vector3 mouse_position = Camera.main.ScreenPointToRay(Input.mousePosition).origin;
             RaycastHit2D hit = Physics2D.Raycast(mouse_position, Vector2.zero);
             if (hit && hit.collider.gameObject == this.gameObject) {
-                click_it();
+                click_it(mouse_position);
             }
         }
     }
 
-    public virtual void click_it() {
-        click_callback.Invoke(this);
+    public virtual void click_it(Vector2 mouse_position) {
+        click_callback.Invoke(this, mouse_position);
     }
 }
--- Untitled Tangram Game/Assets/Code/GameLogic.cs
+++ Untitled Tangram Game/Assets/Code/GameLogic.cs
@@ -10,13 +10,19 @@ public class GameLogic : MonoBehaviour {
     public GUIStyle gizmo_style = new GUIStyle();
 
     private GameObject shape = null;
-    public void clicked(Clickable clickable) {
+    public void clicked(Clickable clickable, Vector2 mouse_position) {
+        /* ================================================================= */
         if (clickable.tag == "Shape") {
             shape = clickable.gameObject;
         } else if (clickable.tag == "Field Piece" && shape != null) {
+            /* ================================================================= */
+            Vector2 offset = mouse_position - (Vector2)clickable.transform.position;
+            offset.x = Mathf.Sign(offset.x);
+            offset.y = Mathf.Sign(offset.y);
+            /* ================================================================= */
             Vector3 new_position = shape.transform.position;
-            new_position.x = clickable.gameObject.transform.position.x;
-            new_position.y = clickable.gameObject.transform.position.y;
+            new_position.x = clickable.gameObject.transform.position.x + 0.5f * offset.x;
+            new_position.y = clickable.gameObject.transform.position.y + 0.5f * offset.y;
             /* ================================================================= */
             shape.transform.position = new_position;
             /* ================================================================= */
