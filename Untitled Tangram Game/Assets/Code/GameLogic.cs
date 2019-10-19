15d5f8cbaf0bcc22d7d942ac8ca8fcf17fb06a0e
Vitalya <vitalya.dev@gmail.com>
2019-10-19 07:07:21 +0300

    Remove 0.5 from code.

--- Untitled Tangram Game/Assets/Code/GameLogic.cs
+++ Untitled Tangram Game/Assets/Code/GameLogic.cs
@@ -15,14 +15,19 @@ public class GameLogic : MonoBehaviour {
         if (clickable.tag == "Shape") {
             shape = clickable.gameObject;
         } else if (clickable.tag == "Field Piece" && shape != null) {
+            float size = 1.0f;
+            if (clickable.GetComponent<BoxCollider2D>()) {
+                size = clickable.GetComponent<BoxCollider2D>().size.x;
+            }
             /* ================================================================= */
             Vector2 offset = mouse_position - (Vector2)clickable.transform.position;
             offset.x = Mathf.Sign(offset.x);
             offset.y = Mathf.Sign(offset.y);
             /* ================================================================= */
-            Vector3 new_position = shape.transform.position;
-            new_position.x = clickable.gameObject.transform.position.x + 0.5f * offset.x;
-            new_position.y = clickable.gameObject.transform.position.y + 0.5f * offset.y;
+            Vector3 new_position = new Vector3();
+            new_position.x = clickable.gameObject.transform.position.x + (size / 2) * offset.x;
+            new_position.y = clickable.gameObject.transform.position.y + (size / 2) * offset.y;
+            new_position.z = shape.transform.position.z;
             /* ================================================================= */
             shape.transform.position = new_position;
             /* ================================================================= */
