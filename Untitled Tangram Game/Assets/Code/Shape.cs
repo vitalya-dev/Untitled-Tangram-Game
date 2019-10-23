4e2f1a0c22790af829152ee9cf2733be1f1a3d4c
Vitalya <vitalya.dev@gmail.com>
2019-10-23 09:07:05 +0300

    Work on Shape Collision

--- Untitled Tangram Game/Assets/Code/Shape.cs
+++ Untitled Tangram Game/Assets/Code/Shape.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 using UnityEngine.Events;
 
 [System.Serializable]
@@ -14,11 +13,16 @@ public class Shape : MonoBehaviour {
     void Start() {
         if (transform.Find("Pivot")) {
             pivot = transform.Find("Pivot").gameObject;
-            pivot.SetActive(false); 
+            pivot.SetActive(false);
         }
     }
 
     void OnTriggerEnter2D(Collider2D col) {
         collide_callback.Invoke(GetComponent<Collider2D>(), col);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        Debug.Log(collision.otherCollider.name);
+        Debug.Log("Collided");
     }
 }
