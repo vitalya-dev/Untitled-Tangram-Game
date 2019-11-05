8d40bd9774d4819503a4267db9ececd518ccbfdf
Vitalya <vitalya.dev@gmail.com>
2019-11-06 00:06:56 +0300

    Okey. Add Background Music.

---
+++ Untitled Tangram Game/Assets/Code/DontDestroy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour {
+    void Awake() {
+        if (GameObject.Find(gameObject.name + "(Loaded)"))
+            GameObject.Destroy(this.gameObject);
+        else {
+            DontDestroyOnLoad(this.gameObject);
+            gameObject.name += "(Loaded)";
+        }
+    }
+}
