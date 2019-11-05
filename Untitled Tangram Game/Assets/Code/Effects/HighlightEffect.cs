4e9da237494307e5dd17e565474b7260cb7f0186
Vitalya <vitalya.dev@gmail.com>
2019-11-05 07:51:29 +0300

    Field Highlight.

---
+++ Untitled Tangram Game/Assets/Code/Effects/HighlightEffect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightEffect : MonoBehaviour {
+    public Color highlight_color;
+    
+    private Color normal_color;
+
+    void Start() {
+        normal_color = GetComponent<SpriteRenderer>().color;
+    }
+    public void highlight(bool value) {
+        GetComponent<SpriteRenderer>().color = value ? highlight_color : normal_color;
+    }
+}
---
+++ Untitled Tangram Game/Assets/Code/Managers/HighlightManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightManager : MonoBehaviour {
+    public void highlight(bool value) {
+        foreach (var highlightable in FindObjectsOfType<HighlightEffect>()) {
+            highlightable.highlight(value);
+        }
+    }
+}
