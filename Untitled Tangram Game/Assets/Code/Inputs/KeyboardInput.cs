0fe211379c7f8e3d8d383cdf4552c2a385600753
Vitalya <vitalya.dev@gmail.com>
2019-11-08 23:07:24 +0300

    Start Working On Pause Menu.

---
+++ Untitled Tangram Game/Assets/Code/Inputs/KeyboardInput.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class KeyboardInput : MonoBehaviour {
+    public UnityEvent on_esc = new UnityEvent();
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape))
+                on_esc.Invoke();
+    }
+}
---
+++ Untitled Tangram Game/Assets/Code/Tumbler.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tumbler : MonoBehaviour {
+    public void tumb() {
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+}
