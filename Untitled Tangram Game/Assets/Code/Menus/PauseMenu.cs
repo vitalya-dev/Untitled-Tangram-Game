dceab00b4a7966a552ce27aae4c2e57112af82b5
Vitalya <vitalya.dev@gmail.com>
2019-11-09 08:06:53 +0300

    Start Menu Done In One Level.

---
+++ Untitled Tangram Game/Assets/Code/Menus/PauseMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+    void OnEnable() {
+        Time.timeScale = 1 - Time.timeScale;
+        foreach (var audio in FindObjectsOfType<AudioSource>()) {
+            audio.pitch = Time.timeScale;
+        }
+    }
+
+    void OnDisable() {
+        Time.timeScale = 1 - Time.timeScale;
+        foreach (var audio in FindObjectsOfType<AudioSource>()) {
+            audio.pitch = Time.timeScale;
+        }
+    }
+}
