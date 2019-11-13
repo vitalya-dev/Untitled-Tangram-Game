a049d0b268e8e5a87d65bef1657a39280c9a9bd2
Vitalya <vitalya.dev@gmail.com>
2019-11-13 23:33:29 +0300

    Lightning Effect Ist Complete.

---
+++ Untitled Tangram Game/Assets/Code/Effects/LightningEffect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningEffect : MonoBehaviour {
+    public ParticleSystem lightning_prefab;
+    private ParticleSystem lightning;
+
+    void Start() {
+         lightning = Instantiate(lightning_prefab, transform.position, Quaternion.identity, transform);
+         lightning.transform.Translate(0, 0, -5, Space.World);
+         if (GetComponent<Selectable>())
+            GetComponent<Selectable>().on_select.AddListener((Selectable s, Vector2 v) => play());
+    }
+
+    public void play() {
+        lightning.Play();
+    }
+}
