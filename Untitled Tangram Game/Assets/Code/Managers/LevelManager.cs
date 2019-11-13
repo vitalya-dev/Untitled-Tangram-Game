ae594a06ffa041ae6190122fd80826e31ae1cbb3
Vitalya <vitalya.dev@gmail.com>
2019-11-13 08:05:28 +0300

    Level Manager Refactor.

--- Untitled Tangram Game/Assets/Code/Managers/LevelManager.cs
+++ Untitled Tangram Game/Assets/Code/Managers/LevelManager.cs
@@ -13,62 +13,44 @@ public class LevelManager : MonoBehaviour {
 
     private AsyncOperation level_loading = null;
 
-     public void level_restart() {
+    public void level_restart() {
         level_restart(0);
     }
 
     public void level_restart(float delay) {
-        StartCoroutine(__level_restart(delay));
-    }
-
-    private IEnumerator __level_restart(float delay) {
-        if (level_loading != null && !level_loading.isDone)
-            yield break;
-        else
-            yield return new WaitForSeconds(delay);
-        /* ==================================== */
-        foreach (var sound in FindObjectsOfType<AudioSource>())
-            if (!sound.GetComponent<DontDestroy>() && sound.isPlaying) {
-                sound.transform.parent = null;
-                DontDestroyOnLoad(sound.gameObject);
-                Destroy(sound.gameObject, 2.0f);
-            }
-        /* ==================================== */
         if (GlobalVariables.attempts < GlobalVariables.max_attempts) {
             GlobalVariables.attempts += 1;
             Scene current_scene = SceneManager.GetActiveScene();
-            level_loading = SceneManager.LoadSceneAsync(current_scene.buildIndex);
+            StartCoroutine(level_load(current_scene.buildIndex, delay));
         } else {
-            level_loading = SceneManager.LoadSceneAsync(1);
-            /* ==================================== */
+            StartCoroutine(level_load(1, delay));
             GlobalVariables.attempts = 0;
         }
     }
 
     public void next_level(float delay) {
-        StartCoroutine(__next_level(delay));
+        GlobalVariables.attempts = 0;
+        /* ==================================== */
+        Scene current_scene = SceneManager.GetActiveScene();
+        if (current_scene.buildIndex + 1 < SceneManager.sceneCountInBuildSettings)
+            StartCoroutine(level_load(current_scene.buildIndex + 1, delay));
+        else
+            StartCoroutine(level_load(1, delay));
     }
 
-    private IEnumerator __next_level(float delay) {
+    private IEnumerator level_load(int index, float delay) {
         if (level_loading != null && !level_loading.isDone)
             yield break;
         else
             yield return new WaitForSeconds(delay);
-
         /* ==================================== */
         foreach (var sound in FindObjectsOfType<AudioSource>())
             if (!sound.GetComponent<DontDestroy>() && sound.isPlaying) {
                 sound.transform.parent = null;
                 DontDestroyOnLoad(sound.gameObject);
-                Destroy(sound.gameObject, 2.0f);
+                Destroy(sound.gameObject, sound.clip.length);
             }
         /* ==================================== */
-        GlobalVariables.attempts = 0;
-        /* ==================================== */
-        Scene current_scene = SceneManager.GetActiveScene();
-        if (current_scene.buildIndex + 1 < SceneManager.sceneCountInBuildSettings)
-            level_loading = SceneManager.LoadSceneAsync(current_scene.buildIndex + 1);
-        else
-            level_loading = SceneManager.LoadSceneAsync(1);
+        level_loading = SceneManager.LoadSceneAsync(index);
     }
 }
