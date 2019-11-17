a665b1de1c2fc18d9d8dec3416e7ac1f1b13cc7b
Vitalya <vitalya.dev@gmail.com>
2019-11-17 11:50:15 +0300

    Dialog System Refactoring.

--- Untitled Tangram Game/Assets/Code/Managers/DialogueManager.cs
+++ Untitled Tangram Game/Assets/Code/Managers/DialogueManager.cs
@@ -7,30 +7,32 @@ using UnityEngine.UI;
 [System.Serializable]
 public class Sentence {
     [TextArea]
-    public string letters;
+    public string text;
     public Sprite avatar;
 }
 
 public class DialogueManager : MonoBehaviour {
     public Canvas ui;
-    public GameObject message_go;
-    
-    public Vector2 start_position;
-    public UnityEvent on_finish;
+    public GameObject sentence_go;
+
+    public Vector2 position;
+
     public Sentence[] sentences;
+
+    public UnityEvent on_finish;
 
     IEnumerator Start() {
         yield return new WaitForSeconds(1.0f);
         for (int i = 0; i < sentences.Length; i++) {
-            Text message_ui = Instantiate(message_go, Vector3.zero, Quaternion.identity, ui.transform).GetComponent<Text>();
-            message_ui.text = sentences[i].letters;
-            message_ui.rectTransform.localPosition = new Vector3(
-                start_position.x,
-                start_position.y - message_ui.rectTransform.rect.height * i,
+            Text sentence_ui = Instantiate(sentence_go, Vector3.zero, Quaternion.identity, ui.transform).GetComponent<Text>();
+            sentence_ui.text = sentences[i].text;
+            sentence_ui.rectTransform.localPosition = new Vector3(
+                position.x,
+                position.y - sentence_ui.rectTransform.rect.height * i,
                 0
             );
-            message_ui.transform.Find("Avatar").GetComponent<Image>().sprite = sentences[i].avatar;
-            message_ui.transform.Find("Avatar").GetComponent<Image>().SetNativeSize();
+            sentence_ui.transform.Find("Avatar").GetComponent<Image>().sprite = sentences[i].avatar;
+            sentence_ui.transform.Find("Avatar").GetComponent<Image>().SetNativeSize();
             yield return new WaitForSeconds(1.0f);
         }
         on_finish.Invoke();
