c85fad7dfb4b6ee10b3b10657c8f5afcaec9c2b9
Vitalya <vitalya.dev@gmail.com>
2019-11-17 11:36:33 +0300

    Dasha And Masha Avatar.

--- Untitled Tangram Game/Assets/Code/Managers/DialogueManager.cs
+++ Untitled Tangram Game/Assets/Code/Managers/DialogueManager.cs
@@ -1,35 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 [System.Serializable]
 public class Sentence {
     [TextArea]
-    public string message;
+    public string letters;
     public Sprite avatar;
 }
 
 public class DialogueManager : MonoBehaviour {
     public Canvas ui;
+    public GameObject message_go;
+    
     public Vector2 start_position;
-    public GameObject message_prefab;
     public UnityEvent on_finish;
     public Sentence[] sentences;
 
     IEnumerator Start() {
         yield return new WaitForSeconds(1.0f);
         for (int i = 0; i < sentences.Length; i++) {
-            Text text_ui = Instantiate(message_prefab, Vector3.zero, Quaternion.identity, ui.transform).GetComponent<Text>();
-            text_ui.text = sentences[i].message;
-            text_ui.rectTransform.localPosition = new Vector3(
+            Text message_ui = Instantiate(message_go, Vector3.zero, Quaternion.identity, ui.transform).GetComponent<Text>();
+            message_ui.text = sentences[i].letters;
+            message_ui.rectTransform.localPosition = new Vector3(
                 start_position.x,
-                start_position.y - text_ui.rectTransform.rect.height * i,
+                start_position.y - message_ui.rectTransform.rect.height * i,
                 0
             );
-            text_ui.transform.Find("Avatar").GetComponent<Image>().sprite = sentences[i].avatar;
-            text_ui.transform.Find("Avatar").GetComponent<Image>().SetNativeSize();
+            message_ui.transform.Find("Avatar").GetComponent<Image>().sprite = sentences[i].avatar;
+            message_ui.transform.Find("Avatar").GetComponent<Image>().SetNativeSize();
             yield return new WaitForSeconds(1.0f);
         }
         on_finish.Invoke();
