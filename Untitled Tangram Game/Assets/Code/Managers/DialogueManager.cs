470c9a0b2539cab6c560a93b9f218a5caf7d4259
Vitalya <vitalya.dev@gmail.com>
2019-11-20 09:05:36 +0300

    Refactoring Personage Messages.

--- Untitled Tangram Game/Assets/Code/Managers/DialogueManager.cs
+++ Untitled Tangram Game/Assets/Code/Managers/DialogueManager.cs
@@ -19,23 +19,24 @@ public class DialogueManager : MonoBehaviour {
     IEnumerator Start() {
         yield return new WaitForSeconds(delay);
 
-        List<Text> sentence_ui_list = new List<Text>();
+        List<Image> sentence_ui_list = new List<Image>();
         for (int i = 0; i < sentences.Length; i++) {
             /* ==================================================== */
-            Text sentence_ui = Instantiate(sentence_go, Vector3.zero, Quaternion.identity, ui.transform).GetComponent<Text>();
+            Image sentence_ui = Instantiate(sentence_go, Vector3.zero, Quaternion.identity, ui.transform).GetComponent<Image>();
             sentence_ui.rectTransform.localPosition = new Vector3(
                 position.x,
-                position.y - sentence_ui.rectTransform.rect.height * i,
+                position.y - (sentence_ui.rectTransform.rect.height + 5) * i,
                 0
             );
+            sentence_ui_list.Add(sentence_ui);
             sentence_ui.transform.Find("Avatar").GetComponent<Image>().sprite = sentences[i].avatar;
             sentence_ui.transform.Find("Avatar").GetComponent<Image>().SetNativeSize();
-            sentence_ui_list.Add(sentence_ui);
             yield return new WaitForSeconds(delay);
             /* ==================================================== */
+            Text text_ui = sentence_ui.transform.Find("Text").GetComponent<Text>();
             foreach (var letter in sentences[i].text) {
-                sentence_ui.text += letter;
-                if (letter == '.' && sentence_ui.text.Length < sentences[i].text.Trim().Length)
+                text_ui.text += letter;
+                if (letter == '.' && text_ui.text.Length < sentences[i].text.Trim().Length)
                     yield return new WaitForSeconds(delay);
                 else
                     yield return null;
