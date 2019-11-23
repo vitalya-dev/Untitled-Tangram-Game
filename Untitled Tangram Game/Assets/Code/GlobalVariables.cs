4f2876cf836cba3fb5f1c358fb1ac7706a640d66
Vitalya <vitalya.dev@gmail.com>
2019-11-23 07:44:24 +0300

    Max Attempts Increased.

--- Untitled Tangram Game/Assets/Code/GlobalVariables.cs
+++ Untitled Tangram Game/Assets/Code/GlobalVariables.cs
@@ -1,6 +1,6 @@
 ﻿
 public static class GlobalVariables {
     public static int attempts = 0;
-    public static int max_attempts = 1;
+    public static int max_attempts = 999;
     public static string last_shot = "None";
 }
