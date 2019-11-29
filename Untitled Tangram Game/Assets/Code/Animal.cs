e0d5030e161a908eeda31b06947dc9abe068cf7a
Vitalya <vitalya.dev@gmail.com>
2019-11-29 09:33:05 +0300

    Animal Is Complete

---
+++ Untitled Tangram Game/Assets/Code/Animal.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Animal : MonoBehaviour {
+    public Sprite animal_is_done;
+
+    void Start() {
+        if (PlayerPrefs.GetString("Reaper", "In Process") == "Is Done")
+            GetComponent<Image>().sprite = animal_is_done;
+    }
+}
---
+++ Untitled Tangram Game/Assets/Code/GameData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Data {
+    public string name;
+    public string value;
+}
+
+public class GameData : MonoBehaviour {
+    public Data[] datas;
+
+    void Start() {
+        foreach (var data in datas) {
+            PlayerPrefs.SetString(data.name, data.value);
+        }
+    }
+}
